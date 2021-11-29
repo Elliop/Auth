6c7e37f2c5dd7b5df2da35284f280fa79aeb580f
Elliop <abdelmouhayminegx@gmail.com>
2021-11-29 19:42:28 +0100

    Add Login route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -74,4 +74,53 @@ router.post(
   }
 );
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.json({
+      errors: [
+        {
+          msg: "Invalids credentials",
+        },
+      ],
+      data: null,
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.json({
+      errors: [
+        {
+          msg: "Invalids credentials",
+        },
+      ],
+      data: null,
+    });
+  }
+
+  const token = JWT.sign(
+    { email: user.email },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: 360000,
+    }
+  );
+
+  return res.json({
+    errors: [],
+    data: {
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    },
+  });
+});
+
 export default router;

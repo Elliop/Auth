d50f8aa1b828f287158729c7fd23cddbf567429a
Elliop <abdelmouhayminegx@gmail.com>
2021-11-30 02:14:01 +0100

    Add some style to logged page

--- client/src/pages/Logged.tsx
+++ client/src/pages/Logged.tsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { UserContext } from "../context";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Logged = () => {
   const [state, setState] = useContext(UserContext);
   const navigate = useNavigate();
 
+  console.log(state.data?.username);
   const handleLogout = () => {
     setState({ data: null, loading: false, error: null });
     localStorage.removeItem("token");
@@ -14,15 +15,21 @@ const Logged = () => {
 
   return (
     <>
-      <div className="flex justify-end my-5 shadow-md">
+      <div className="flex justify-end items-center my-5 shadow-sm">
         <button
           onClick={handleLogout}
-          className="font-semibold text-blue-600 text-lg mr-6 mb-3"
+          className="bg-indigo-500 text-gray-100 p-2 w-24 rounded-full tracking-wide
+                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                shadow-lg mr-6 mb-3"
         >
           Logout
         </button>
       </div>
-      <div>Hello</div>
+      <div className="flex justify-center mt-8">
+        <h1 className="text-base font-semibold">
+          Hello {state.data?.username} you are now logged to the app!
+        </h1>
+      </div>
     </>
   );
 };

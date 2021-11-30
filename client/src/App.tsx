44f4ee5468077047d68d42cd60d919f0c40c86ed
Elliop <abdelmouhayminegx@gmail.com>
2021-11-30 01:36:30 +0100

    mv App to Home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,9 @@
-import { useState } from "react";
-import Forgot from "./components/Forgot";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
+import Home from "./pages/Home";
 
 const App = () => {
-  const [login, setLogin] = useState(true);
-  const [signUp, setSignUp] = useState(false);
-  const [forgot, setForgot] = useState(false);
-
-  const showLogin = () => {
-    setLogin(true);
-    setSignUp(false);
-    setForgot(false);
-  };
-  const showSignUp = () => {
-    setSignUp(true);
-    setLogin(false);
-    setForgot(false);
-  };
-  const showForgot = () => {
-    setForgot(true);
-    setSignUp(false);
-    setLogin(false);
-  };
   return (
     <>
-      {login && <Login onSignUp={showSignUp} onForgot={showForgot} />}
-      {signUp && <SignUp onLogin={showLogin} />}
-      {forgot && <Forgot onLogin={showLogin} onSignUp={showSignUp} />}
+      <Home />
     </>
   );
 };

ade1f8e1b7dba1ece5004c6d10a0bc4bd30cc36e
Elliop <abdelmouhayminegx@gmail.com>
2021-11-30 01:39:18 +0100

    Add routing and protect logged page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+import Logged from "./pages/Logged";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/logged" element={<ProtectedRoute />}>
+          <Route path="/logged" element={<Logged />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 

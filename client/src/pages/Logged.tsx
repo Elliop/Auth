d11689e53f65f7b0afac996aa5b2a74bf46bb9c4
Elliop <abdelmouhayminegx@gmail.com>
2021-11-30 02:02:19 +0100

    Add logOut button in logged page

--- client/src/pages/Logged.tsx
+++ client/src/pages/Logged.tsx
@@ -1,9 +1,29 @@
+import { useContext } from "react";
+import { UserContext } from "../context";
+import { Link, useNavigate } from "react-router-dom";
+
 const Logged = () => {
+  const [state, setState] = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setState({ data: null, loading: false, error: null });
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
-    <div>
-      {/*  */}
-      hello
-    </div>
+    <>
+      <div className="flex justify-end my-5 shadow-md">
+        <button
+          onClick={handleLogout}
+          className="font-semibold text-blue-600 text-lg mr-6 mb-3"
+        >
+          Logout
+        </button>
+      </div>
+      <div>Hello</div>
+    </>
   );
 };
 
